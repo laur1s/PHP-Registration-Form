ec02dd6d5b12c974ebabc27f04657e3375a2362d
Laurynas Tumosa <l.tumosa@gmail.com>
2018-09-06 16:52:11 +0100

    cleanup

--- assets/js/tos.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#reg').click(function (e) {
-        if ($('#TOS').not(':checked').length) {
-            e.preventDefault();
-            alert('You must agree with terms of service');
-        }
-
-    })
-});
