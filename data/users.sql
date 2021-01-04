b86eb94dcfc0c08eaed274ed53f07b122e39873b
Yakim Rachev <yakim.rachev@endava.com>
2021-01-04 16:50:38 +0200

    Adding sample DB data containing an admin user

--- data/users.sql
+++ data/users.sql
@@ -29,13 +29,34 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS db;
 USE db;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+CREATE TABLE IF NOT EXISTS users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  is_admin BOOLEAN,
+  first_name varchar(100) NOT NULL,
+  sir_name varchar(100) NOT NULL,
+  title ENUM('Mr.', 'Mrs.'),
+  country varchar(100),
+  city varchar(100),
+  email varchar(100) NOT NULL,
+  password varchar(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE (email)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+
+INSERT IGNORE INTO users (id, is_admin, first_name, sir_name, email, password) 
+VALUES 
+(1, true, 'Admin', 'Automation', 'admin@automation.com', '9b8769a4a742959a2d0298c36fb70623f2dfacda8436237df08d8dfd5b37374c');
+
+INSERT IGNORE INTO users (is_admin, first_name, sir_name, email, title, country, city, password) 
+VALUES 
+(false, 'Ivan', 'Dimotrov', 'idimitrov@automation.com', 'Mr.', 'Bulgaria', 'Sofia', '9b8769a4a742959a2d0298c36fb70623f2dfacda8436237df08d8dfd5b37374c'),
+(false, 'Yoana', 'Ivanova', 'yivanova@automation.com', 'Mrs.', 'Bulgaria', 'Sopot', '9b8769a4a742959a2d0298c36fb70623f2dfacda8436237df08d8dfd5b37374c'),
+(false, 'Zdravka', 'Petrova', 'zpetrova@automation.com', 'Mrs.', 'Bulgaria', 'Elin Pelin', '9b8769a4a742959a2d0298c36fb70623f2dfacda8436237df08d8dfd5b37374c'),
+(false, 'Todor', 'Ivanov', 'tivanov@automation.com', 'Mr.', 'Bulgaria', 'Pravets', '9b8769a4a742959a2d0298c36fb70623f2dfacda8436237df08d8dfd5b37374c'),
+(false, 'Zahari', 'Avramov', 'zavramov@automation.com', 'Mr.', 'Bulgaria', 'Kardjali', '9b8769a4a742959a2d0298c36fb70623f2dfacda8436237df08d8dfd5b37374c');
+
+
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
