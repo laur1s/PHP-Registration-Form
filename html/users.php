951bb40c8f4b22d2e056a45eebc06d2b408c58bd
Yakim Rachev <yakim.rachev@endava.com>
2021-01-08 14:31:15 +0200

    Adding Sorting to Users list

--- html/users.php
+++ html/users.php
@@ -1,16 +1,37 @@
 <?php
-ob_start();
-session_start();
-require_once 'dbconnect.php';
+    ob_start();
+    session_start();
+    require_once 'dbconnect.php';
 
-if (!isset($_SESSION['user'])) {
-    header("Location: login.php");
-    exit;
-}
-// select logged in users detail
-$res = $conn->query("SELECT * FROM users WHERE id=" . $_SESSION['user']);
-$userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
+    if (!isset($_SESSION['user'])) {
+        header("Location: login.php");
+        exit;
+    }
+    // select logged in users detail
+    $res = $conn->query("SELECT * FROM users WHERE id=" . $_SESSION['user']);
+    $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
+
+    // Table sorting mechanism
+
+    // For extra protection these are the columns of which the user can sort by (in your database table).
+    $columns = array('title', 'first_name', 'sir_name', 'country', 'city', 'email');
+    
+    // Only get the column if it exists in the above columns array, if it doesn't exist the database table will be sorted by the first item in the columns array.
+    $column = isset($_GET['column']) && in_array($_GET['column'], $columns) ? $_GET['column'] : $columns[0];
+
+    // Get the sort order for the column, ascending or descending, default is ascending.
+    $sort_order = isset($_GET['order']) && strtolower($_GET['order']) == 'desc' ? 'DESC' : 'ASC';
+
+    // Get the result...
+    if ($result = $conn->query("SELECT * FROM users  WHERE is_admin = false ORDER BY " .  $column . " " . $sort_order)) {
+        // Some variables we need for the table.
+        $up_or_down = str_replace(array('ASC','DESC'), array('by-attributes','by-attributes-alt'), $sort_order); 
+        $asc_or_desc = $sort_order == 'ASC' ? 'desc' : 'asc';
+        $add_class = ' class="highlight"';
+    
+    }
 ?>
+
 
 <!DOCTYPE html>
 <head>
@@ -32,7 +53,7 @@ $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Website Name</a>
+                <a class="navbar-brand" href="#">Endava SoA SUT</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
@@ -64,15 +85,52 @@ $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
     <h2>Available users</h2>
     <p>&nbsp;</p>
     <p>&nbsp;</p>
-    <table class="table table-striped" id="users_list" class="col-md-8">
+    <table class="table table-striped table-bordered" id="users_list" class="col-md-8">
         <thead>
             <tr>
-            <th scope="col">Title</th>
-            <th scope="col">First Name</th>
-            <th scope="col">Sir Name</th>
-            <th scope="col">Country</th>
-            <th scope="col">City</th>
-            <th scope="col">Email</th>
+                <th scope="col">
+                    <a href="users.php?column=title&order=<?php echo $asc_or_desc; ?>">
+                    Title
+                    <i class="glyphicon glyphicon-sort<?php echo $column == 'title' ? '-' . $up_or_down : ''; ?>"></i>
+                    </a>
+                </th>
+
+                <th scope="col">
+                    <a href="users.php?column=first_name&order=<?php echo $asc_or_desc; ?>">
+                    First Name
+                    <i class="glyphicon glyphicon-sort<?php echo $column == 'first_name' ? '-' . $up_or_down : ''; ?>"></i>
+                    </a>
+                </th>
+
+                <th scope="col">
+                    <a href="users.php?column=sir_name&order=<?php echo $asc_or_desc; ?>">
+                    Sir Name
+                    <i class="glyphicon glyphicon-sort<?php echo $column == 'sir_name' ? '-' . $up_or_down : ''; ?>"></i>
+                    </a>
+                </th>
+
+                <th scope="col">
+                    <a href="users.php?column=country&order=<?php echo $asc_or_desc; ?>">
+                    Country
+                    <i class="glyphicon glyphicon-sort<?php echo $column == 'country' ? '-' . $up_or_down : ''; ?>"></i>
+                    </a>
+                </th>
+
+                <th scope="col">
+                    <a href="users.php?column=city&order=<?php echo $asc_or_desc; ?>">
+                    City
+                    <i class="glyphicon glyphicon-sort<?php echo $column == 'city' ? '-' . $up_or_down : ''; ?>"></i>
+                    </a>
+                </th>
+
+                
+                <th scope="col">
+                    <a href="users.php?column=email&order=<?php echo $asc_or_desc; ?>">
+                    Email
+                    <i class="glyphicon glyphicon-sort<?php echo $column == 'email' ? '-' . $up_or_down : ''; ?>"></i>
+                    </a>
+                </th>
+
             <?php
                 if ($userRow['is_admin']){
                     echo "<th scope='col'>Action</th>";
@@ -82,9 +140,9 @@ $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
         </thead>
         <tbody>
             <?php 
-                $usersRes = $conn->query("SELECT * FROM users WHERE is_admin = false");
+                // $usersRes = $conn->query("SELECT * FROM users WHERE is_admin = false");
 
-                while($row = mysqli_fetch_array($usersRes, MYSQLI_ASSOC)){
+                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
                     echo "<tr>";
                     echo "<td>".$row['title']."</td>";
                     echo "<td>".$row['first_name']."</td>";
@@ -105,7 +163,6 @@ $userRow = mysqli_fetch_array($res, MYSQLI_ASSOC);
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
-
 </body>
 </html>
 
